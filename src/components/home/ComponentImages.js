afd27962849d23a920ec61528379f8468b514578
EduardXI <eduardo.salazar@agenciaservitec.com>
2022-08-05 16:51:05 -0500

    add ComponentImages confing

--- src/components/home/ComponentImages.js
+++ src/components/home/ComponentImages.js
@@ -1,23 +1,40 @@
-import React, { useState } from "react";
+import React from "react";
+import styled from "styled-components";
 
-export const ComponentImages = () => {
-  const title = "juan"; // constante de prueba xd
-  const [titulo, setTitulo] = useState("");
-  const newTitle = () => {
-    setTitulo(title);
-  };
-  const imagesArray = [img, dad, dasdas, dasda]; // ejemplo de array xd
+export const ComponentImages = (props) => {
+  // const { title } = props; // props para el titulo
+  // const { imagesArray } = props;//props para el array de imagenes
+
+  // --------- prueba de array para que no se buggee porque no tiene props que traer por ahora------//
+  const title = "juanEjemplo";
+  const imagesArray = ["dsa", "dsad", "dasd", "dasd"]; // ejemplo de array xd
 
   return (
     <Container>
       <WrapperTitle>
-        <h1>{newTitle}</h1>
+        {/*<h1>{title}</h1>*/}
+        <h1>{title}</h1>
       </WrapperTitle>
       <WrapperImg>
         {imagesArray.map((imageArray, index) => (
-          <img key={index} src={imageArray} />
+          <img key={index} src={imageArray} alt="no hay nada" />
         ))}
       </WrapperImg>
     </Container>
   );
 };
+
+const Container = styled.div`
+  width: 100%;
+`;
+
+const WrapperTitle = styled.div`
+  padding: 1.5rem;
+  text-align: center;
+`;
+
+const WrapperImg = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
