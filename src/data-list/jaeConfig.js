93576f7cf68383b787afb560d8beee096c9c2752
angelgt <angel.gamonal@agenciaservitec.com>
2022-08-05 17:45:00 -0500

    add imcomplete jaeConfig

---
+++ src/data-list/jaeConfig.js
@@ -0,0 +1,40 @@
+export const jaeConfig = {
+  header: {
+    title: "Jefatura de Apoyo Educativo",
+    logoImg: "",
+    menuHeader: ["Inicio", "Nosotros", "Colegios"],
+  },
+  main: {
+    sectionCarousel: {
+      title: "JEFATURA DE ADMINISTRACION DE CASAS DEL EJÉRCITO.",
+      imgBackground: "",
+      buttons: [
+        {
+          title: "Nosotros",
+        },
+        {
+          title: "Requisitos",
+        },
+      ],
+    },
+    sectionAbout: {
+      title: "Nosotros",
+      subTitle:
+        "Planificamos y evaluamos, los procesos educativos e instrucción, en el Comando  de Educación y Doctrina del Ejército.",
+      description: [
+        "Supervisar y controlar la ejecución de los programas educativos de las escuelas en el Comando de Educación y  Doctrina del Ejército, de conformidad a la normatividad vigente",
+        "Proporcionar el soporte académico para las actividades a desarrollar por la Jefatura de Educación del Comando de Educación y Doctrina del Ejército, en lo concerniente a los recursos humanos, organización, logísticos y financieros.",
+      ],
+    },
+    sectionOther: {
+      title: "Otras por orden del CG del Ejército o del CG del COEDE.",
+      img: "",
+    },
+    SectionSchools: {
+      title: "Instituciones de Lima y Provincia:",
+      limaSchools: [],
+      provinceSchools: [],
+    },
+  },
+  footer: {},
+};
