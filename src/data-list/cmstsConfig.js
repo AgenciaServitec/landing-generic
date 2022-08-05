577e2608d3b1146ee7a7ee23597c8868cc4645a4
angelgt <angel.gamonal@agenciaservitec.com>
2022-08-05 17:42:56 -0500

    add cmstsConfig

---
+++ src/data-list/cmstsConfig.js
@@ -0,0 +1,92 @@
+import {
+  CmstsCarousel,
+  CmstsCollage1,
+  CmstsCollage2,
+  CmstsCollage3,
+  CmstsSedeChorillos1,
+  CmstsSedeChorillos2,
+  CmstsSedeTarapaca,
+  CmstsServices,
+  CobieneLogoLarge,
+} from "../images";
+
+export const cmstsConfig = {
+  header: {
+    title: "Círculo Militar Supervisores Técnicos de Sub Oficiales",
+    logoImg: CobieneLogoLarge,
+    menuHeader: ["Servicios", "Sedes"],
+  },
+  main: {
+    sectionCarousel: {
+      title: "Círculo Militar Supervisores Técnicos de Sub Oficiales.",
+      imgBackground: CmstsCarousel,
+      buttons: [
+        {
+          title: "Nuestros Servicios",
+        },
+        {
+          title: "Sedes",
+        },
+      ],
+    },
+    sectionServices: {
+      title: "DISFRUTA LO QUE BRINDA LA ASOCIACIÓN CMSTS",
+      list: [
+        "SALONES PARA EVENTOS.",
+        "RESTAURANTE.",
+        "AROBICOS.",
+        "KARATE.",
+        "CANCHAS SINTETICA.",
+        "ZONA DE PARRILLAS.",
+        "HOSPEDAJE.",
+        "PISCINA OLIMPICA/SEMIOLIMPICA.",
+        "PAINTBALL",
+      ],
+      image: CmstsServices,
+    },
+    sectionCampus: [
+      {
+        title: "SEDE CHORRILLOS",
+        content: [
+          {
+            image: CmstsSedeChorillos1,
+            description:
+              "CONTAMOS CON UN SALÓN PRINCIPAL Y ÁREAS PARA REALIZAR TODO TIPO DE EVENTOS. ADEMÁS DE TENER DISPONIBLE LA PISCINA SEMI OLÍMPICA.",
+          },
+          {
+            image: CmstsSedeChorillos2,
+            description:
+              "CONTAMOS CON UNA TERRAZA, UN SALÓN MULTIUSO, LA CABAÑA DEL GUERRERO, SALÓN ALFA Y BRAVO PARA REALIZAR TODO TIPO DE EVENTOS Y CANCHAS DEPORTIVAS DE MATERIAL SINTÉTICO.",
+          },
+        ],
+      },
+      {
+        title: "SEDE TARAPACA",
+        content: [
+          {
+            image: CmstsSedeTarapaca,
+            description:
+              "CONTAMOS CON ÁREAS PARA REALIZAR TODO TIPO DE EVENTOS. ADEMÁS DE TENER DISPONIBLE LA PISCINA OLÍMPICA, RESTAURANTE Y CANCHAS DEPORTIVAS DE MATERIAL SINTÉTICO.",
+          },
+          {
+            image: CmstsSedeTarapaca,
+            description:
+              "CONTAMOS CON LOS AMBIENTES IDEALES PARA REALIZAR TODO TIPO DE EVENTOS, HOSPEDAJE Y JUEGOS DEPORTIVOS PAINTBALL.",
+          },
+        ],
+      },
+      {
+        title: "SEDE RIMAC",
+        content: [
+          {
+            image: CmstsSedeTarapaca,
+            description:
+              "Cuenta con los ambientes ideales para realizar todo tipo de eventos. Además de tener disponible la piscina semi olímpica y cancha deportiva de material sintético.",
+          },
+        ],
+      },
+    ],
+    sectionCollage: [CmstsCollage1, CmstsCollage2, CmstsCollage3],
+  },
+  footer: {},
+};
