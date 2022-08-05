e24252cba33d0dcc16e52ed187f44ee275634fc8
angelgt <angel.gamonal@agenciaservitec.com>
2022-08-05 17:43:21 -0500

    add export config

--- src/data-list/index.js
+++ src/data-list/index.js
@@ -1,1 +1,5 @@
 export { default as phoneCodes } from "./phoneCodes.json";
+export { jaceConfig } from "./jaceConfig";
+export { jaeConfig } from "./jaeConfig";
+export { sadConfig } from "./sadConfig";
+export { cmstsConfig } from "./cmstsConfig";
