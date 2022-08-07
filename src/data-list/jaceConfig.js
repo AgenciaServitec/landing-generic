9bc344c7f436ba991cdc90549a00435b0010286d
angelgt <angel.gamonal@agenciaservitec.com>
2022-08-07 18:43:07 -0500

    update jaceConfig.js

--- src/data-list/jaceConfig.js
+++ src/data-list/jaceConfig.js
@@ -6,6 +6,12 @@ import {
   JaceServices,
   SaedSlider1,
 } from "../images";
+import { faFacebook, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import {
+  faEnvelope,
+  faGlobe,
+  faMobile,
+} from "@fortawesome/free-solid-svg-icons";
 
 export const jaceConfig = {
   header: {
@@ -14,8 +20,8 @@ export const jaceConfig = {
     logoImg: CobieneLogoLarge,
     menuList: [
       { id: "about-us", name: "Nosotros" },
-      { id: "requirements", name: "Requisitos" },
-      { id: "", name: "Tramites" },
+      { id: "information", name: "Requisitos" },
+      { id: "contact", name: "Contáctanos" },
     ],
   },
   main: {
@@ -25,9 +31,13 @@ export const jaceConfig = {
         imgBackground: SaedSlider1,
         buttons: [
           {
+            type: "primary",
+            id: "about-us",
             title: "Nosotros",
           },
           {
+            type: "secondary",
+            id: "contact",
             title: "Requisitos",
           },
         ],
@@ -35,106 +45,99 @@ export const jaceConfig = {
     ],
     sectionAbout: {
       title: "Nosotros",
-      description:
+      descriptions: [
         "La JACE asesorar al Comando de Bienestar del Ejército en los aspectos inherentes de la administración de casas de servicios del Ejército. Asimismo, planificara, ejecutara y supervisara los trabajos de conservación, mejoramiento y rehabilitación de la infraestructura, inmobiliaria de las casas de servicio de la guarnición de Lima, a fin de contribuir a la consecución de los objetivos de Bienestar del Ejército.",
-      img: JaceAbout,
-    },
-    /*   sectionServices: [
-      {
-        img: "",
-        title: "LABOR CONSTANTE",
-      },
-      {
-        img: "",
-        title: "JACE",
-      },
-      {
-        img: "",
-        title:
-          "ESTA JEFATURA DE ADMINISTRACIÓN DE CASAS DEL EJERCITO SIGUE TRABAJANDO PARA EL BIENESTAR DE NUESTRA FAMILIA MILITAR",
-      },
-    ],*/
-    sectionServices: {
-      card: [
-        {
-          img: JaceServices,
-        },
       ],
+      buttons: [{ type: "secondary", name: "CONTÁCTANOS" }],
+      images: [JaceAbout],
+    },
+    sectionServices: {
+      title: "",
+      images: [JaceServices],
     },
     sectionRequirements: [
       {
-        img: "",
+        image: "",
         title: "Paso 1",
         description: [
           "Deberá encontrarse en situación militar de actividad con no más de 25 años para Oficiales y 28 años para Tcos y Sub Oficiales.",
         ],
       },
       {
-        img: "",
+        image: "",
         title: "Paso 2",
         description: [
           "Deberá ser casado y tener hijos con su conyugue, mantener una relación estable y vivir con su conyugue en la guarnición donde solicita la casa.",
         ],
       },
       {
-        img: "",
+        image: "",
         title: "Paso 3",
         description: [
           "Deberá tener como mínimo 4 años de servicio cumplido como Oficial o Sub Oficial, con el grado mínimo de Teniente o Sub Oficial 2o.",
         ],
       },
       {
-        img: "",
+        image: "",
         title: "Paso 4",
         description: [
           "El personal militar que proviene de la reserva puede solicitar asignación de casa de servicio a partir de la fecha que pasen a la efectividad",
         ],
       },
       {
-        img: "",
+        image: "",
         title: "Paso 5",
         description: [
           "No deben haber sido favorecidos con un préstamo de Ores –Fovime.",
         ],
       },
       {
-        img: "",
+        image: "",
         title: "Paso 6",
         description: [
           "No debe haber ocupado casa de servicio en los 3 últimos años en la guarnición donde solicita.",
         ],
       },
       {
-        img: "",
+        image: "",
         title: "Paso 7",
         description: [
           "El personal militar que proviene de la reserva puede solicitar asignación de casa de servicio a partir de la fecha que pasen a la efectividad.",
         ],
       },
       {
-        img: "",
-        title: "Paso 8",
-        description: [
-          "El personal militar candidato a usuario no debe haber sido obligado a desocupar la casa de servicio en cualquiera de las guarniciones de Ejercito donde existan casas de servicio.",
-        ],
-      },
-      {
-        img: "",
-        title: "Paso 9",
-        description: [
-          "El personal militar candidato a “usuario” que solicite asignación especial o excepcional deberá contar con:",
-          "a.Los documentos que acrediten la situación de especialidad o excepcionalidad.",
-          "b.El dictamen legal favorable.",
-          "c.La opinión del Crl Ing Jefe de la DACE.",
-          "d.La aprobación del General de Brigada Comandante General del COBIENE, para la guarnición de Lima",
-        ],
+        title:
+          "FLUJOGRAMA PARA LA ASIGNACION DE CASA DE SERVICIO EN LA JEFATURA DE ADMINISTRACION DE CASAS DEL EJERCITO (JACE)",
+        images: [JaceMap, JaceMap2],
       },
     ],
-    sectionConceptualMap: {
-      title:
-        "FLUJOGRAMA PARA LA ASIGNACION DE CASA DE SERVICIO EN LA JEFATURA DE ADMINISTRACION DE CASAS DEL EJERCITO (JACE)",
-      img: JaceMap,
-      imgTwo: JaceMap2,
+    sectionContact: {
+      title: "CONTACT COBIENE",
+      subtitle: "No dudes en comunicarte",
+      socialsRed: [
+        {
+          href: "https://api.whatsapp.com/send/?phone=+51941801827&text=Estoy+interesado+en+su+producto+%2F+servicio&app_absent=0",
+          icon: faWhatsapp,
+          name: "Whatsapp",
+          phone: "941801827",
+        },
+        {
+          href: "mailto:contactos@servitec-peru.com",
+          icon: faEnvelope,
+          name: "Email",
+        },
+        {
+          href: "https://www.facebook.com/Servitec.chorrillos/",
+          icon: faFacebook,
+          name: "Facebook",
+        },
+        { href: "https://youtu.be/FNMfFy4iKvg", icon: faMobile, name: "Phone" },
+        {
+          href: "https://youtu.be/FNMfFy4iKvg",
+          icon: faGlobe,
+          name: "Direción",
+        },
+      ],
     },
   },
   footer: "",
