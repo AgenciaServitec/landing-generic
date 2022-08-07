edcf88581b7cfa5671234fbf51f5a7f3d3bcd7e2
noelmoriano <noel.moriano@webuild.pe>
2022-08-07 17:21:26 -0500

    refactoring code

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,42 +1,15 @@
 import React from "react";
 import styled from "styled-components";
-import {
-  AboutUs,
-  Banner,
-  Carousel,
-  Contact,
-  ListItems,
-} from "../../components";
-import { AgrariaLogo, CientificaDelSur } from "../../images";
+import { AboutUs, Carousel, Contact, ListItems } from "../../components";
 import { ComponentImages } from "../../components/home/ComponentImages";
 import { useTemplateConfig } from "../../providers";
 import { saedConfig } from "../../data-list";
-
-const temporalListConvenions = [
-  {
-    title: "Agraria",
-    image: AgrariaLogo,
-  },
-  {
-    title: "Cientifica del Sur",
-    image: CientificaDelSur,
-  },
-  {
-    title: "Agraria",
-    image: AgrariaLogo,
-  },
-  {
-    title: "Cientifica del Sur",
-    image: CientificaDelSur,
-  },
-];
 
 export const Home = () => {
   const { templateConfig } = useTemplateConfig();
 
   const sectionAboutUs = templateConfig.main.sectionAbout;
   const sectionComponentImages = templateConfig.contentImages;
-
   const cardsConventions = saedConfig.main.sectionConvenios;
   const cardsInstitutes = saedConfig.main.sectionInstitutes;
 
@@ -44,25 +17,33 @@ export const Home = () => {
     <>
       <Container>
         <Carousel carouselItems={templateConfig.main.sectionCarousel} />
-        <AboutUs
-          title={sectionAboutUs.title}
-          descriptions={sectionAboutUs.descriptions}
-          buttons={sectionAboutUs.buttons}
-          images={sectionAboutUs.images}
-        />
-        <ComponentImages
-          title={sectionComponentImages.title}
-          images={sectionComponentImages.images}
-        />
+        {sectionAboutUs && (
+          <AboutUs
+            title={sectionAboutUs.title}
+            descriptions={sectionAboutUs.descriptions}
+            buttons={sectionAboutUs.buttons}
+            images={sectionAboutUs.images}
+          />
+        )}
+        {sectionComponentImages && (
+          <ComponentImages
+            title={sectionComponentImages.title}
+            images={sectionComponentImages.images}
+          />
+        )}
         {/*<Banner />*/}
-        <ListItems
-          title={cardsConventions.title}
-          items={cardsConventions.cards}
-        />
-        <ListItems
-          title={cardsInstitutes.title}
-          items={cardsInstitutes.cards}
-        />
+        {cardsConventions && (
+          <ListItems
+            title={cardsConventions.title}
+            items={cardsConventions.cards}
+          />
+        )}
+        {cardsInstitutes && (
+          <ListItems
+            title={cardsInstitutes.title}
+            items={cardsInstitutes.cards}
+          />
+        )}
         <Contact />
       </Container>
     </>
