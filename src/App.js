b68b29f1f8df52924375347fdf077e17089fec74
noelmoriano <noel.moriano@webuild.pe>
2022-08-07 12:51:53 -0500

    add get theme and template

--- src/App.js
+++ src/App.js
@@ -6,8 +6,26 @@ import { GlobalStyles } from "./styles/themes/GlobalStyle";
 import { ThemeProvider } from "styled-components";
 import { themes } from "./styles";
 import { setLocale } from "yup";
+import {
+  cmstsConfig,
+  jaceConfig,
+  jaeConfig,
+  sadConfig,
+  saedConfig,
+} from "./data-list";
+
+const templateConfigs = {
+  cmsts: cmstsConfig,
+  jace: jaceConfig,
+  jae: jaeConfig,
+  sad: sadConfig,
+  saed: saedConfig,
+  saeco: saedConfig,
+  default: saedConfig,
+};
 
 export const App = () => {
+  const [templateType, setTemplateType] = useState("default");
   const [themeType, setThemeType] = useState("default");
 
   const hostName = window.location.hostname;
@@ -15,20 +33,45 @@ export const App = () => {
   useEffect(() => {
     return () => {
       setLocale(yup["es"]);
-      setThemeType(getThemeType());
+      getThemeAndTemplateConfig();
     };
   }, []);
 
-  const getThemeType = () => {
+  const getThemeAndTemplateConfig = () => {
     switch (hostName) {
-      case "saeco.cobiene.mil.pe":
-        return "primary";
-      case "saed.cobiene.mil.pe":
-        return "primary";
-      default:
-        return "default";
+      case "cmsts.cobiene.mil.pe": {
+        setTemplateType("cmsts");
+        return setThemeType("primary");
+      }
+      case "jace.cobiene.mil.pe": {
+        setTemplateType("jace");
+        return setThemeType("primary");
+      }
+      case "jae.cobiene.mil.pe": {
+        setTemplateType("jae");
+        return setThemeType("primary");
+      }
+      case "sad.cobiene.mil.pe": {
+        setTemplateType("sad");
+        return setThemeType("primary");
+      }
+      case "saed.cobiene.mil.pe": {
+        setTemplateType("saed");
+        return setThemeType("primary");
+      }
+      case "saeco.cobiene.mil.pe": {
+        setTemplateType("saeco");
+        return setThemeType("primary");
+      }
+      default: {
+        setTemplateType("saed");
+        return setThemeType("default");
+      }
     }
   };
+
+  console.log("template->", templateConfigs[templateType]);
+  console.log("theme->", themes[themeType]);
 
   return (
     <BrowserRouter>
