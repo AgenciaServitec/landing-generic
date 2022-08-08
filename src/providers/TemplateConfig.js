c4b055aea27b97041819028c4d604c916d1336d8
noelmoriano <noel.moriano@webuild.pe>
2022-08-07 20:34:49 -0500

    update template config

--- src/providers/TemplateConfig.js
+++ src/providers/TemplateConfig.js
@@ -23,7 +23,7 @@ const TemplateConfigContext = createContext({
 });
 
 export const TemplateConfig = ({ children }) => {
-  const [templateType, setTemplateType] = useState("saeco");
+  const [templateType, setTemplateType] = useState("default");
 
   const hostName = window.location.hostname;
 
