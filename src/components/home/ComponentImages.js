83d888de67e83dcc603541d44d536e34e6c35b38
angelgt <angel.gamonal@agenciaservitec.com>
2022-08-08 17:43:01 -0500

    refactoring ComponentImages

--- src/components/home/ComponentImages.js
+++ src/components/home/ComponentImages.js
@@ -2,19 +2,29 @@ import React from "react";
 import styled from "styled-components";
 import { mediaQuery } from "../../styles/constants/mediaQuery";
 
-export const ComponentImages = ({ title, images }) => {
+const wrapperImgType = {
+  primary: {
+    gridTemplateColumns: "1fr 1fr",
+  },
+  secondary: {
+    gridTemplateColumns: "1fr",
+  },
+};
+
+export const ComponentImages = ({ title, images, type = "primary" }) => {
   // const { title } = props; // props para el titulo
   // const { imagesArray } = props;//props para el array de imagenes
-
   // --------- prueba de array para que no se buggee porque no tiene props que traer por ahora------//
+
+  const wrapperImgStyle = wrapperImgType[type];
 
   return (
     <Container>
       <WrapperTitle>
         {/*<h1>{title}</h1>*/}
-        {{ title } ? <h1>{title}</h1> : ""}
+        {{ title } ? <h2>{title}</h2> : ""}
       </WrapperTitle>
-      <WrapperImg>
+      <WrapperImg gridTemplateColumns={wrapperImgStyle.gridTemplateColumns}>
         {images.map((image, index) => (
           <Img key={index} src={image} alt={title} />
         ))}
@@ -32,25 +42,29 @@ const Container = styled.div`
 const WrapperTitle = styled.div`
   padding: 1.5rem;
   text-align: center;
+  h2 {
+    font-weight: bold;
+    font-size: 1.5rem;
+    ${mediaQuery.minTablet} {
+      font-size: 2.5rem;
+      line-height: 3rem;
+    }
+  }
 `;
 
 const WrapperImg = styled.div`
   padding: 0.5rem 0.5rem 1rem 0.5rem;
-  background-repeat: no-repeat;
   display: grid;
   margin-bottom: 1.7rem;
-  grid-template-rows: 1fr 1fr;
   grid-template-columns: 1fr;
   ${mediaQuery.minTablet} {
-    grid-template-rows: 1fr;
-    grid-template-columns: 1fr 1fr;
+    grid-template-columns: ${({ gridTemplateColumns }) => gridTemplateColumns};
   }
-  //display: flex;
-  //justify-content: center;
 `;
 
 const Img = styled.img`
   width: 100%;
-  background-repeat: no-repeat;
-  padding: 0.5rem;
+  max-width: 65rem;
+  padding: 1rem;
+  margin: auto;
 `;
