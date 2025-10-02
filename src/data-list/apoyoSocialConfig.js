a13ab4385c0493709f4cfa75ff682618795b8bb6
Jean <jean.servitec@gmail.com>
2025-10-02 11:39:08 -0500

    UPDATE CONVENIOS

--- src/data-list/apoyoSocialConfig.js
+++ src/data-list/apoyoSocialConfig.js
@@ -1814,7 +1814,7 @@ export const apoyoSocialConfig = {
                 "Ofrecerá un 10% de descuento sobre el valor neto de las tarifas, tanto para personales de acuerdo a elección del BENEFICIARIO, siendo válido para vuelos nacionales como internacionales.",
                 "El descuento aplica sobre la tarifa base sin incluir impuestos ni servicios adicionales y no es aplicable sobre tarifas promocionales",
                 "Para acceder a la compra de los pasajes, lo podrán realizar en la pagina web <a href='https://www.skyairline.com/es/peru' target='_blank'>https://www.skyairline.com/es/peru</a> , debiendo tomar en cuenta las siguientes condiciones: ",
-                "a). Al momento de hacer la compra deberán ingresar el “promocode” asignado (este promocode cambiará mensualmente y será difundido por el departamento de apoyo social del comando de bienestar del ejército), promocode Agosto 2025 : SKYEJERCITO14 valido del 01/10/2025 hasta el 31/10/2025",
+                "a). Al momento de hacer la compra deberán ingresar el “promocode” asignado (este promocode cambiará mensualmente y será difundido por el departamento de apoyo social del comando de bienestar del ejército), promocode OCTUBRE 2025 : SKYEJERCITO14 valido del 01/10/2025 hasta el 31/10/2025",
                 "b). Es requisito fundamental que el miembro del ejercito del perù, sea uno de los pasajeros de la reserva sobre la cual se aplica el descuento."
               ],
             },
