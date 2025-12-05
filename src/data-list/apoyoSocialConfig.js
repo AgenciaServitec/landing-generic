31b60150ee10e54e3bde8ee6ae189272e9162312
Jean <jean.servitec@gmail.com>
2025-12-05 11:34:22 -0500

    UPDATE CONVENIOS WEB - FALTA APP #2

--- src/data-list/apoyoSocialConfig.js
+++ src/data-list/apoyoSocialConfig.js
@@ -1573,7 +1573,7 @@ export const apoyoSocialConfig = {
                 "Ofrecerá un 10% de descuento sobre el valor neto de las tarifas, tanto para personales de acuerdo a elección del BENEFICIARIO, siendo válido para vuelos nacionales como internacionales.",
                 "El descuento aplica sobre la tarifa base sin incluir impuestos ni servicios adicionales y no es aplicable sobre tarifas promocionales",
                 "Para acceder a la compra de los pasajes, lo podrán realizar en la pagina web <a href='https://www.skyairline.com/es/peru' target='_blank'>https://www.skyairline.com/es/peru</a> , debiendo tomar en cuenta las siguientes condiciones: ",
-                "a). Al momento de hacer la compra deberán ingresar el “promocode” asignado (este promocode cambiará mensualmente y será difundido por el departamento de apoyo social del comando de bienestar del ejército), promocode Noviembre 2025 : SKYEJERCITO15 valido del 01/11/2025 hasta el 30/11/2025",
+                "a). Al momento de hacer la compra deberán ingresar el “promocode” asignado (este promocode cambiará mensualmente y será difundido por el departamento de apoyo social del comando de bienestar del ejército), promocode Diciembre 2025 : SKYEJERCITO16 valido del 01/12/2025 hasta el 31/12/2025",
                 "b). Es requisito fundamental que el miembro del ejercito del perù, sea uno de los pasajeros de la reserva sobre la cual se aplica el descuento."
               ],
             },
