9205625abbdf1b837f57aaeee550a6d9ce498f31
Jean <jean.servitec@gmail.com>
2026-02-16 14:31:28 -0500

    template

--- src/providers/TemplateConfig.js
+++ src/providers/TemplateConfig.js
@@ -29,8 +29,7 @@ const TemplateConfigContext = createContext({
 export const TemplateConfig = ({ children }) => {
   const [templateType, setTemplateType] = useState("default");
 
-  const hostName = "saed.jbiene.com";
-  // const hostName = window.location.hostname;
+  const hostName = window.location.hostname;
 
   useEffect(() => {
     switch (hostName) {
